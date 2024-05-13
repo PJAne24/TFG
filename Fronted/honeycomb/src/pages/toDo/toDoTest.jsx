a55423d6446d8636f39a02d210af01f127088b30
PJane24 <jhoannacpd@gmail.com>
2024-05-13 22:28:07 +0200

    ultimos cambios de hoy

--- Fronted/honeycomb/src/pages/toDo/toDoTest.jsx
+++ Fronted/honeycomb/src/pages/toDo/toDoTest.jsx
@@ -1,46 +1,52 @@
-import './toDoTest.css'
+import React from 'react';
+import './toDoTest.css';
 
-const toDoTest = () => {
+const ToDoTest = () => {
     return (
-        <>
         <div className='container'>
             <div className='uno'>
                 <div className="hexagonBack">
                     <div className='hexagon'>
+                        <span className="titleHexagon">Profile</span>
                     </div>
                 </div>
                 <div className="hexagonBack">
                     <div className='hexagon'>
+                        <span className="titleHexagon">ToDo</span>
                     </div>
                 </div>
                 <div className="hexagonBack">
                     <div className='hexagon'>
+                        <span className="titleHexagon">Tasks</span>
                     </div>
                 </div>
             </div>
             <div className='dos'>
                 <div className="hexagonBack">
                     <div className='hexagon'>
+                        <span className="titleHexagon">Calendar</span>
                     </div>
                 </div>
                 <div className="hexagonBack">
                     <div className='hexagon'>
+                        <span className="titleHexagon">Credits</span>
                     </div>
                 </div>
             </div>
             <div className='dos' id="dos-dos">
                 <div className="hexagonBack">
                     <div className='hexagon'>
+                        <span className="titleHexagon">Tasks</span>
                     </div>
                 </div>
                 <div className="hexagonBack">
                     <div className='hexagon'>
+                        <span className="titleHexagon">Calendar</span>
                     </div>
                 </div>
             </div>
         </div>
-        </>
     );
 }
 
-export default toDoTest;
+export default ToDoTest;
